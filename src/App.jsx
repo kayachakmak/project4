e306734456cfd16217cec0c69819e2b76ed9c367
Kaya Chakmak <kycakmak@gmail.com>
2023-11-01 10:55:16 +0100

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,45 @@ import Form from "./components/Form/Form";
 import { uid } from "uid";
 import List from "./components/Form/List/List";
 import useLocalStorage from "use-local-storage";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   const [activities, setActivites] = useLocalStorage("activities", []);
-  const [isGoodweather,setIsGoodweather] = useState(true);
-  
+  const [weather, setWeather] = useState({});
+
+  const URL = "https://example-apis.vercel.app/api/weather";
+
+  useEffect(() => {
+    async function Fetch() {
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        console.log(data);
+        setWeather(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    Fetch();
+  }, []);
+
+  console.log(weather.isGoodWeather);
   function handleAddActivity(newActivity) {
     setActivites([{ ...newActivity, id: uid() }, ...activities]);
   }
 
-  
-   const filteredActivities= activities.filter((activity) => {
-    return activity.isForGoodweather === isGoodweather})
+  const filteredActivities = activities.filter((activity) => {
+    return activity.isForGoodweather === weather.isGoodWeather;
+  });
 
   return (
     <>
-      <List activities={filteredActivities} isGoodweather={isGoodweather} />
+      <Header condition={weather.condition} temperature={weather.temperature} />
+      <List
+        activities={filteredActivities}
+        isGoodWeather={weather.isGoodWeather}
+      />
       <Form onAddActivity={handleAddActivity} />
     </>
   );
--- src/components/Form/List/List.jsx
+++ src/components/Form/List/List.jsx
@@ -1,17 +1,17 @@
 import { Fragment } from "react";
 
 /* eslint-disable react/prop-types */
-export default function List({ activities,isGoodweather }) {
+export default function List({ activities, isGoodWeather }) {
   return (
     <Fragment>
-    <p>{isGoodweather? 'Weather is good': 'Weather is shitty'} </p>
-    <ul>
-      {activities.map((activity) => (
-        <li key={activity.id} className="activity">
-          {activity.name}
-        </li>
-      ))}
-    </ul>
+      <p>{isGoodWeather ? "Weather is good" : "Weather is shitty"} </p>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id} className="activity">
+            {activity.name}
+          </li>
+        ))}
+      </ul>
     </Fragment>
   );
 }
